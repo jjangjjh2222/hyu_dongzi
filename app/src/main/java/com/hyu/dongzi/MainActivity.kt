1873ca2f02615e0db69516d4c61fbd0f9ce6ee9a
jjangjjh2222 <88773219+jjangjjh2222@users.noreply.github.com>
2021-08-15 15:45:00 +0900

    코드부분 수정했어요 ~

--- app/src/main/java/com/hyu/dongzi/MainActivity.kt
+++ app/src/main/java/com/hyu/dongzi/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.hyu.dongzi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyu.dongzi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) { //oncreat = 앱이 최초 실행되었을 때 수행한다.
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root) //xml 화면 뷰를 연결한다.
+
+        btn_register.setOnClickListener {
+
+            val intent = Intent(this, RegActivity::class.java )
+            startActivity(intent)
+
+        }
     }
 }
