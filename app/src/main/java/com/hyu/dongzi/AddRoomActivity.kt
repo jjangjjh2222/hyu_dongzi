721cf90017b76a03a275aefd1594f543757446dd
jjangjjh2222 <88773219+jjangjjh2222@users.noreply.github.com>
2021-08-18 17:56:28 +0900

    데이터베이스에 등록

--- app/src/main/java/com/hyu/dongzi/AddRoomActivity.kt
+++ app/src/main/java/com/hyu/dongzi/AddRoomActivity.kt
@@ -5,25 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import com.google.firebase.database.FirebaseDatabase
 import com.hyu.dongzi.databinding.ActivityAddRoomBinding
 import kotlinx.android.synthetic.main.activity_add_room.*
 import kotlinx.android.synthetic.main.fragment_user.*
 
 class AddRoomActivity : AppCompatActivity() {
-//    private var uid:String?= ""
+    private var uid:String?= ""
     val binding by lazy { ActivityAddRoomBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) { //onCreate = 앱이 최초 실행되었을 때 수행한다.
         super.onCreate(savedInstanceState)
         setContentView(binding.root) //xml 화면 뷰를 연결한다.
 
-//        if(intent.hasExtra("uid")){
-//
-//            uid = intent.getStringExtra("uid")
-//        }
+        if(intent.hasExtra("uid")){
+            uid = intent.getStringExtra("uid")
+        }
 
+        btn_addRoom.setOnClickListener {
 
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference()
+
+            val dataInput = Rooms(
+                btn_add_image.imageAlpha.toInt(),
+                et_deposit.text.toString(),
+                et_monthly.text.toString(),
+                toggleButton3.text.toString()
+            )
+            myRef.child(uid!!).push().setValue(dataInput)
+        }
     }
-
     fun loadImage(view: View) {
         startActivityForResult(
             Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI),
--- app/src/main/java/com/hyu/dongzi/RoomsActivity.kt
+++ app/src/main/java/com/hyu/dongzi/RoomsActivity.kt
@@ -3,6 +3,7 @@ package com.hyu.dongzi
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.hyu.dongzi.databinding.ActivityRoomsBinding
 import kotlinx.android.synthetic.main.activity_add_room.*
@@ -10,10 +11,8 @@ import kotlinx.android.synthetic.main.activity_rooms.*
 import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class RoomsActivity : AppCompatActivity() {
-//    private var uid:String?= ""
+    private lateinit var auth : FirebaseAuth
     val binding by lazy { ActivityRoomsBinding.inflate(layoutInflater) }
-
-
 //     listView 에 들어갈 정보들을 Rooms 클래스에 맞춰 roomsList 라는 List 에 담음
     val roomsList = arrayListOf<Rooms>(
         Rooms(R.drawable.ic_launcher_foreground, "300만원", "30만원", "원룸"),
@@ -32,33 +31,18 @@ class RoomsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root) //xml 화면 뷰를 연결한다.
 
-//        if(intent.hasExtra("uid")){
-//
-//            uid = intent.getStringExtra("uid")
-//        }
+        auth = FirebaseAuth.getInstance()
 
         val adapter = roomsAdapter(this, roomsList)
         ListView.adapter = adapter
 
         fab_add_room.setOnClickListener {
             val intent = Intent(this, AddRoomActivity::class.java)
+            intent.putExtra("uid", auth.currentUser?.uid)
             startActivity(intent)
         }
 
-//        btn_addRoom.setOnClickListener {
-//
-//            val database = FirebaseDatabase.getInstance()
-//            val myRef = database.getReference()
-//
-//            val dataInput = Rooms(
-//                btn_add_image.imageAlpha.toInt(),
-//                et_deposit.text.toString(),
-//                et_monthly.text.toString(),
-//                toggleButton3.text.toString()
-//
-//            )
-//
-//            myRef.child(uid!!).push().setValue(dataInput)
-//        }
+
+
+        }
     }
-}
