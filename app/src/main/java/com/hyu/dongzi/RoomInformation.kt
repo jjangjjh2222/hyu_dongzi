c474b2d100509bf53eadbc43c9c768536a64742d
widfers <widfers@naver.com>
2021-08-18 15:33:33 +0900

    등록하기 버튼 추가

---
+++ app/src/main/java/com/hyu/dongzi/RoomInformation.kt
@@ -0,0 +1,11 @@
+package com.hyu.dongzi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_information)
+    }
+}
