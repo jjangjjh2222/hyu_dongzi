7ed6ef40685ff98208a7d042223051810420f76c
jjangjjh2222 <88773219+jjangjjh2222@users.noreply.github.com>
2021-08-22 19:07:54 +0900

    chat perpect

--- app/src/main/java/com/hyu/dongzi/RoomInformationActivity.kt
+++ app/src/main/java/com/hyu/dongzi/RoomInformationActivity.kt
@@ -5,31 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ImageView
-import android.widget.ListView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.core.view.View
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import com.hyu.dongzi.ChatList.ChatListAdapter
 import com.hyu.dongzi.ChatList.ChatListFragment
 import com.hyu.dongzi.ChatList.ChatListItem
 import com.hyu.dongzi.DBKey.Companion.CHILD_CHAT
-import com.hyu.dongzi.DBKey.Companion.DB_ARTICLES
 import com.hyu.dongzi.DBKey.Companion.DB_USERS
+import com.hyu.dongzi.chatdetail.ChatRoomActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.btn_register
+import kotlinx.android.synthetic.main.activity_room_information.*
+import kotlinx.android.synthetic.main.item_chatlist.*
 import kotlinx.android.synthetic.main.room_item.*
 
 class RoomInformationActivity : AppCompatActivity() {
-
-    private lateinit var articleDB: DatabaseReference
     private lateinit var userDB: DatabaseReference
-    private lateinit var roomListAdapter: RoomListAdapter
-
+    lateinit var chatListAdapter: ChatListAdapter
 
     private val auth: FirebaseAuth by lazy {
         Firebase.auth
@@ -66,72 +66,42 @@ class RoomInformationActivity : AppCompatActivity() {
 
             }
         })
+        btn_chat.setOnClickListener {
+            val intent = Intent(this, ChatRoomActivity::class.java)
+            startActivity(intent)
+        }
 
-
-//        val lv = findViewById<ListView>(R.id.lv_roomsList)
-//        lv.adapter = adapter
+//        btn_chat.setOnClickListener {
+//            val lc = findViewById<RecyclerView>(R.id.chatListRecyclerView)
+//            lc.adapter = chatListAdapter
 //
-//        lv.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-//            if (auth.currentUser != null) {
-////                // 로그인 상태;
-////                // 채팅방 생성
-//                val selectItem = parent.getItemAtPosition(position) as Room
-//                val intent = Intent(this, ChatListFragment::class.java)
-//
-//                intent.putExtra("buyerId", auth.currentUser?.uid.toString())
-//                intent.putExtra("sellerId", selectItem.uid)
-//                intent.putExtra("id", selectItem.id)
+//            lc.setOnClickListener {
+//                AdapterView.OnItemClickListener { parent, view, position, id ->
 //
 //
-//
-//                startActivity(intent)
-//
-//
-//            }
-//        }
-
-
-//    private fun initArticleAdapter(view: View) {
-//        roomListAdapter = RoomListAdapter(onItemClicked = { Room ->
-//            if(auth.currentUser != null){
-//                // 로그인 상태;
-//                if(auth.currentUser?.uid != Room.uid){
-//                    // 채팅방 생성
-//                    val chatRoom = ChatListItem(
-//                        buyerId = auth.currentUser?.uid.toString(),
-//                        sellerId = Room.uid,
-//                        key = System.currentTimeMillis()
-//                    )
+//                    val selectItem = parent.getItemAtPosition(position) as ChatListItem
 //
 //                    userDB.child(auth.currentUser!!.uid) // 계속 워닝 떠서 !! 처리;
 //                        .child(CHILD_CHAT)
 //                        .push()
-//                        .setValue(chatRoom)
-//
-//                    userDB.child(Room.sellerId)
-//                        .child(CHILD_CHAT)
-//                        .push()
-//                        .setValue(chatRoom)
-//
-//                    Snackbar.make(view, "채팅방이 생성되었습니다. 채팅탭에서 확인해주세요.", Snackbar.LENGTH_LONG).show()
-//
-//                }else{
-//                    // 내가 올린 아이템 일때
-//                    Snackbar.make(view, "내가 올린 아이템입니다.", Snackbar.LENGTH_LONG).show()
-//                }
-//            }else{
-//                // 로그아웃 상태;
-//                Snackbar.make(view, "로그인 후 사용해주세요", Snackbar.LENGTH_LONG).show()
-//            }
+//                        .setValue(chatRoomTitleTextView)
 //
 //
-//        })
-//    }
+//
+//                    userDB = Firebase.database.reference.child(DB_USERS)
 
-//    private fun initDB() {
-//        articleDB = Firebase.database.reference.child(DB_ARTICLES) // 디비 가져오기;
-//        userDB = Firebase.database.reference.child(DB_USERS)
-//    }
+//                    val intent = Intent(this, ChatRoomActivity::class.java)
+//
+//                    intent.putExtra("buyerId", auth.currentUser?.uid.toString())
+//                    intent.putExtra("sellerId", selectItem.sellerId)
+//
+//                    startActivity(intent)
+
+//                }
+//            }
+//        }
     }
+
 }
+
 
