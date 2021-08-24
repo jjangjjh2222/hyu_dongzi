d81f4fe13e6db3044f32a1fe566b559e6a700fb8
jjangjjh2222 <88773219+jjangjjh2222@users.noreply.github.com>
2021-08-25 01:58:03 +0900

    good

---
+++ app/src/main/java/com/hyu/dongzi/ContractListAdapter.kt
@@ -0,0 +1,4 @@
+package com.hyu.dongzi
+
+class ContractListAdapter {
+}
--- app/src/main/java/com/hyu/dongzi/SaveContractActivity.kt
+++ app/src/main/java/com/hyu/dongzi/SaveContractActivity.kt
@@ -2,12 +2,51 @@ package com.hyu.dongzi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hyu.dongzi.databinding.ActivitySavecontractBinding
-class SaveContractActivity : AppCompatActivity(){
-    val binding by lazy { ActivitySavecontractBinding.inflate(layoutInflater) }
-    override fun onCreate(savedInstanceState: Bundle?) { //oncreat = 앱이 최초 실행되었을 때 수행한다.
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root) //xml 화면 뷰를 연결한다.
+import android.util.Log
+import android.widget.ListView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
+class SaveContractActivity : AppCompatActivity(){
+
+    lateinit var adapter : RoomListAdapter
+    val list = mutableListOf<Room>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_savecontract)
+
+        adapter = RoomListAdapter(list)
+        val lv = findViewById<ListView>(R.id.lv_contractList)
+        lv.adapter = adapter
+
+        getData()
+    }
+
+    fun getData() {
+        val database = Firebase.database
+        val myRef = database.getReference("board")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children) {
+
+                    val item = dataModel.getValue(Room::class.java)
+                    list.add(item!!)
+
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("RoomsActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        myRef.addValueEventListener(postListener)
     }
 }
