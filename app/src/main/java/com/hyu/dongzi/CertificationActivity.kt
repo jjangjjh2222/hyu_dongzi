4a62c40e8b453691d31d65a6787c1647fad4f13b
widfers <widfers@naver.com>
2021-08-25 20:58:41 +0900

    .

--- app/src/main/java/com/hyu/dongzi/CertificationActivity.kt
+++ app/src/main/java/com/hyu/dongzi/CertificationActivity.kt
@@ -1,16 +1,37 @@
 package com.hyu.dongzi
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.hyu.dongzi.navigation.UserFragment
 import kotlinx.android.synthetic.main.activity_certification.*
+import org.jetbrains.anko.startActivityForResult
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CertificationActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 2
+    lateinit var currentPhotoPath : String
 
     var auth = Firebase.auth
     val uid = auth.currentUser?.uid.toString()
@@ -20,6 +41,14 @@ class CertificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_certification)
+
+        setPermission()
+
+        iv_idCard.setOnClickListener {
+
+            takePicture()
+
+        }
 
         btn_verify.setOnClickListener {
 
@@ -31,5 +60,75 @@ class CertificationActivity : AppCompatActivity() {
             Toast.makeText(this, "인증이 완료되었습니다", Toast.LENGTH_SHORT).show()
 
         }
+
     }
+
+    private fun takePicture() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex : IOException) {
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.textbutton.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    private fun createImageFile(): File {
+        val timestamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPGE_${timestamp}_", ".jpg", storageDir)
+            .apply { currentPhotoPath = absolutePath }
+    }
+
+    private fun setPermission() {
+        val permission = object : PermissionListener {
+            override fun onPermissionGranted() {
+                Toast.makeText(this@CertificationActivity, "권한이 허용되었습니다", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@CertificationActivity, "권한이 거부되었습니다", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        TedPermission.with(this)
+            .setPermissionListener(permission)
+            .setRationaleMessage("카메라를 사용하기위해 권한 허용이 필요합니다")
+            .setDeniedMessage("권한을 거부하셨습니다")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+            .check()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val bitmap: Bitmap
+            val file = File(currentPhotoPath)
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
+                iv_idCard.setImageBitmap(bitmap)
+            } else {
+                val decode = ImageDecoder.createSource(
+                    this.contentResolver,
+                    Uri.fromFile(file)
+                )
+                bitmap = ImageDecoder.decodeBitmap(decode)
+                iv_idCard.setImageBitmap(bitmap)
+            }
+        }
+    }
+
 }
