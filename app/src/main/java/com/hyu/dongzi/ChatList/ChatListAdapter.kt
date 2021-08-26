3051d0a8d63a889d070f8e63f80c7271ee10055f
widfers <widfers@naver.com>
2021-08-26 21:39:22 +0900

    chat

--- app/src/main/java/com/hyu/dongzi/ChatList/ChatListAdapter.kt
+++ app/src/main/java/com/hyu/dongzi/ChatList/ChatListAdapter.kt
@@ -9,6 +9,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.hyu.dongzi.R
@@ -29,19 +33,40 @@ class ChatListAdapter(val List: MutableList<ChatListItem>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
-
-
         var view = convertView
 
-
+        val database = Firebase.database
 
         if (view == null) {
             view = LayoutInflater.from(parent?.context).inflate(R.layout.item_chatlist, parent, false)
         }
 
-        val sellerId = view?.findViewById<TextView>(R.id.sellerId)
+        val sellerName = view?.findViewById<TextView>(R.id.sellerId)
 
-        sellerId?.text = List[position].roomId
+
+        // 사용자 이름 받아오기
+        database.getReference("board").child(List[position].roomId).child("uid")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val uid = snapshot.value.toString()
+
+                    // 사용자 이름 받아오기
+                    database.getReference("users").child(uid).child("name")
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                val value = snapshot.value.toString()
+                                sellerName?.text = value
+
+                            }
+                            override fun onCancelled(error: DatabaseError) {
+                            }
+                        })
+
+                }
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+
 
         val storageReference = Firebase.storage.reference.child(List[position].roomId + ".png")
         val image = view?.findViewById<ImageView>(R.id.imageView5)
